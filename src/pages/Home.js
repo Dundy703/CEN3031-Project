f5746b2ea0417b485417027b8040504240c89eef
g4v1ng <gmgravedeperalta@gmail.com>
2024-03-04 05:31:40 -0500

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import Image from '../assets/shop.jpg';
+import '../styles/Home.css'
 
 function Home() {
-  return (
-    <div>Hello World</div>
-  )
+    return (
+        <div className="home" style={{ backgroundImage: `url(${Image})`}}>
+            <div className="infoBox">
+                <h1> MingleMart </h1>
+                <p> A groundbreaking E-commerce platform </p>
+                <Link to="/signup">
+                    <button>Sign up</button>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default Home
