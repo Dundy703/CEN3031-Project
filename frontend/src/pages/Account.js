9160dd5b97ee8a9c4e342dafc65b437d7ae94175
BuildTools <gmgravedeperalta@gmail.com>
2024-04-23 19:45:00 -0400

    fix refresh bug

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigation } from 'react-router-dom'
 
 function Account() {
+    let navigate = useNavigation();
     function logout() {
         localStorage.removeItem("token");
         window.location.reload();
+
     }
     return (
 
